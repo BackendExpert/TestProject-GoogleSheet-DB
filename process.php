0fc2e783205ad89168b434c41fec0c752bde76ee
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-09-28 15:17:49 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ process.php
@@ -0,0 +1,30 @@
+<?php
+
+$mysqli = new mysqli("localhost", "database_name", "username", "dbpassword");
+// Check connection
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+    exit();
+
+} else {
+    if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['mobile']) && isset($_POST['message'])) {
+        $name = htmlspecialchars(trim($_POST['name']));
+        $email = htmlspecialchars(trim($_POST['email']));
+        $mobile = htmlspecialchars(trim($_POST['mobile']));
+        $msg = htmlspecialchars(trim($_POST['message']));
+    }
+
+    $sql = "INSERT INTO contact(name,email,mobile,message) VALUES ('" . addslashes($name) . "', '" . addslashes($email) . "','" . addslashes($mobile) . "', '" . addslashes($msg) . "')";
+
+    if (mysqli_query($mysqli, $sql)) {
+
+        echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+              <strong>Thank you!</strong> You you have sent message successfullly we will get back to you soon...
+              <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>';
+
+    } else {
+        echo "something went wrong!.." . $message;
+    }
+    // echo "data submit  in database";
+}
